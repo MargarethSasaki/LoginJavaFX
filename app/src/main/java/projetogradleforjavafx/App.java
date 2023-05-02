ede42f07b846f274e03a117cf7e4dbc18ab0978b
Margareth Sasaki <116614775+MargarethSasaki@users.noreply.github.com>
2023-05-02 20:13:51 -0300

    Update App.java
    Adicionado os imports

--- app/src/main/java/projetogradleforjavafx/App.java
+++ app/src/main/java/projetogradleforjavafx/App.java
@@ -1,12 +1,17 @@
 package projetogradleforjavafx;
  
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.Label;
+
  
 public class App extends Application {
     
